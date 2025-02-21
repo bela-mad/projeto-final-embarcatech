85ef9d8660d542fe3d778d515d679f8fe6755b5f
bela-mad <isabelaargolo3110@gmail.com>
2025-02-21 00:00:27 -0300

    Inversão da ordem de configuração dos pinos do joystick

--- include/joystick.h
+++ include/joystick.h
@@ -6,13 +6,13 @@
 #include "hardware/adc.h"
 
 // Defines
-#define VRX 27        
 #define VRY 26      
+#define VRX 27        
 #define ADC_CHANNEL_0 0
 #define ADC_CHANNEL_1 1
 
 // Cabeçalhos das funções
-void joystick_read_axis(uint16_t *vrx_value, uint16_t *vry_value);
 void setup_joystick();
+void joystick_read_axis(uint16_t *vrx_value, uint16_t *vry_value);
 
 #endif
