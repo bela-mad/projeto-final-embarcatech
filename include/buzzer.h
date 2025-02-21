e90062e409e40b801123878c0e2f904b4371136f
bela-mad <isabelaargolo3110@gmail.com>
2025-02-20 23:18:21 -0300

    Configuração simples do buzzer

---
+++ include/buzzer.h
@@ -0,0 +1,13 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+// Defines
+#define BUZZER_A_PIN 21
+
+// Cabeçalhos das funções
+
+#endif
