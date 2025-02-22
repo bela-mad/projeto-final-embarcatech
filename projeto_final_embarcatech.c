d10d2afa56e09d32e90a4eac900e7595dfb16ae1
bela-mad <isabelaargolo3110@gmail.com>
2025-02-22 17:43:10 -0300

    Adição de comentários, impressão de dados e teste de uso de condicionais

--- projeto_final_embarcatech.c
+++ projeto_final_embarcatech.c
@@ -16,10 +16,12 @@
 
 // Declaração de variáveis
 ssd1306_t ssd;
-uint32_t last_time = 0;                              // variável para contar o último tempo verificado
+uint32_t last_time = 0;                                         // variável para contar o último tempo verificado
 // absolute_time_t debounce;
-uint16_t vrx_value, vry_value;                       // variáveis dos valores mai recentes de x e y do joystick
-uint slice_led_r, slice_led_g, slice_led_b;          // variáveis para armazenar os slices de PWM correspondentes aos LEDs
+uint16_t vrx_value, vry_value;                                  // variáveis dos valores mai recentes de x e y do joystick
+char buffer_umid[16], buffer_chuva[16];                         // Variáveis de buffer para armazenar os valores como string
+uint slice_led_r, slice_led_g, slice_led_b;                     // variáveis para armazenar os slices de PWM correspondentes aos LEDs
+uint16_t nivel_chuva = 0, nivel_umidade = 0, aceleracao = 0;    // variáveis para armazenar os valores de precipitação, umidade do solo e aceleração do solo
 
 int main() {
 
@@ -28,9 +30,9 @@ int main() {
     // Inicialização dos pinos
     setup_joystick();
     button_init(BUTTON_A_PIN);
-    pwm_led_setup(RED_LED_PIN, &slice_led_r);  // configura o PWM para o LED vermelho
+    pwm_led_setup(RED_LED_PIN, &slice_led_r);    // configura o PWM para o LED vermelho
     pwm_led_setup(GREEN_LED_PIN, &slice_led_g);  // configura o PWM para o LED verde
-    pwm_led_setup(BLUE_LED_PIN, &slice_led_b); // configura o PWM para o LED azul
+    pwm_led_setup(BLUE_LED_PIN, &slice_led_b);   // configura o PWM para o LED azul
 
     // Inicialização do I2C e do display OLED
     i2c_init(I2C_PORT, 400 * 1000);                               // usando-o em 400KHz
@@ -45,6 +47,7 @@ int main() {
     // debounce = delayed_by_ms(get_absolute_time(), 200); // inicializa o debounce
     
     // Limpa o display, desenha a borda e a mensagem de boas-vindas e envia os dados para serem exibidos
+    // MODIFICAR MENSAGEM
     ssd1306_fill(&ssd, false);
     ssd1306_rect(&ssd, 2, 2, 124, 60, true);
     ssd1306_draw_string(&ssd, "Bem-vindo", 30, 8);
@@ -58,33 +61,113 @@ int main() {
         sleep_ms(50);  // Pequena espera para evitar uso excessivo de CPU
     }
 
-    // Limpa o display, desenha a borda e envia os dados para serem exibidos
+    // Limpa o display, desenha a borda e uma mensagem de aguardo e envia os dados para serem exibidos
     ssd1306_fill(&ssd, false);
     ssd1306_rect(&ssd, 2, 2, 124, 60, true);
+    ssd1306_draw_string(&ssd, "Aguarde um", 28, 19);
+    ssd1306_draw_string(&ssd, "momento ...", 20, 35);
     ssd1306_send_data(&ssd);
 
     while (true) {
 
         joystick_read_axis(&vrx_value, &vry_value); // lê os valores dos eixos do joystick
 
-        // Chuva fraca: abaixo de 5,0 mm/h
-        // Chuva moderada: entre 5,0 e 25 mm/h
-        // Chuva forte: entre 25,1 e 50 mm/h
-        // Chuva muito forte: acima de 50,0 mm/h
+        // Umidade baixa: Menos de 20% (baixo risco)
+        // Umidade média: Entre 21 e 40% (risco moderado)
+        // Umidade alta: Acima de 41-60% (alto risco)
+        // Umidade crítica: Acima de 61% (risco crítico)
 
-        ssd1306_draw_string(&ssd, "Umidade: 100%", 10, 8);
-        ssd1306_draw_string(&ssd, "Chuva: 15mm@", 10, 19);
-        ssd1306_draw_string(&ssd, "Desloc: 20m/s", 10, 30);
-        ssd1306_draw_string(&ssd, "Risco Baixo", 20, 46);
+        nivel_umidade = vrx_value / 40.95; // (4096 / 100%)
 
-        for (int ciclo_atv = 200; ciclo_atv <= 3000; ciclo_atv += 30) {
-            pwm_set_duty_cycle_rgb(ciclo_atv, 209, 2, 2);
-            sleep_ms(10);
+        // Chuva fraca: abaixo de 5,0 mm 
+        // Chuva moderada: entre 5,0 e 25 mm 
+        // Chuva forte: entre 26 e 50 mm 
+        // Chuva muito forte: acima de 50,0 e até 150 mm (limite do aparelho)
+        
+        nivel_chuva = vry_value / 27.3; // (4096 / 150mm)
+
+        snprintf(buffer_umid, sizeof(buffer_umid), "%d%%", nivel_umidade);
+        snprintf(buffer_chuva, sizeof(buffer_chuva), "%dmm", nivel_chuva);
+
+        // RISCO BAIXO E LED VERDE
+        if (nivel_umidade < 20 || nivel_chuva < 5) {
+
+            // Define risco
+            ssd1306_draw_string(&ssd, "Risco Baixo", 20, 46);
+
+            // Define cor do LED
+            for (int ciclo_atv = 200; ciclo_atv <= 3000; ciclo_atv += 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 13, 201, 0);
+                sleep_ms(10);
+            }
+            for (int ciclo_atv = 3000; ciclo_atv >= 200; ciclo_atv -= 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 13, 201, 0);
+                sleep_ms(10);
+            }
+
+            // // Manda mensagem para a central
+            // printf("DADOS COLETADOS\n");
+            // printf("Umidade: %d\n", nivel_umidade);
+            // printf("Chuva: %d\n", nivel_chuva);
+            // printf("Aceleração: %d\n", aceleracao);
         }
-        for (int ciclo_atv = 3000; ciclo_atv >= 200; ciclo_atv -= 30) {
-            pwm_set_duty_cycle_rgb(ciclo_atv, 209, 2, 2);
-            sleep_ms(10);
+
+        // RISCO MODERADO E LED AMARELO
+        if (nivel_umidade > 21 && nivel_umidade < 40 && nivel_chuva > 5 && nivel_chuva < 26) {
+            ssd1306_draw_string(&ssd, "Risco Moderado", 20, 46);
+
+            for (int ciclo_atv = 200; ciclo_atv <= 3000; ciclo_atv += 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 255, 230, 0);
+                sleep_ms(10);
+            }
+            for (int ciclo_atv = 3000; ciclo_atv >= 200; ciclo_atv -= 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 255, 230, 0);
+                sleep_ms(10);
+            }
         }
+
+        // RISCO ALTO E LED LARANJA
+        if (nivel_umidade > 41 && nivel_umidade < 60 && nivel_chuva > 26 && nivel_chuva < 50) {
+            ssd1306_draw_string(&ssd, "Risco Moderado", 20, 46);
+
+            for (int ciclo_atv = 200; ciclo_atv <= 3000; ciclo_atv += 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 255, 230, 0);
+                sleep_ms(10);
+            }
+            for (int ciclo_atv = 3000; ciclo_atv >= 200; ciclo_atv -= 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 255, 230, 0);
+                sleep_ms(10);
+            }
+        }
+
+        // RISCO CRÍTICO E LED VERMELHO
+        if (nivel_umidade >= 61 || nivel_chuva > 50) {
+            ssd1306_draw_string(&ssd, "Risco Crítico", 20, 46);
+
+            for (int ciclo_atv = 200; ciclo_atv <= 3000; ciclo_atv += 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 209, 2, 2);
+                sleep_ms(10);
+            }
+            for (int ciclo_atv = 3000; ciclo_atv >= 200; ciclo_atv -= 30) {
+                pwm_set_duty_cycle_rgb(ciclo_atv, 209, 2, 2);
+                sleep_ms(10);
+            }
+        }
+        
+        // if (vry_value >= 2400 && vry_value <= 2120) {
+            //     led_r_level = abs(vrx_value - 2047) * 2;
+            // } 
+            // else if { // caso contrário, mantém o level em 0
+            //     led_r_level = 0;
+            // }
+
+        ssd1306_fill(&ssd, false); 
+        ssd1306_rect(&ssd, 2, 2, 124, 60, true);   
+        ssd1306_draw_string(&ssd, "Umidade: ", 10, 8);
+        ssd1306_draw_string(&ssd, buffer_umid, 75, 8);  // Desenha a string no display
+        ssd1306_draw_string(&ssd, "Chuva: ", 10, 19);
+        ssd1306_draw_string(&ssd, buffer_chuva, 65, 19);  // Desenha a string no display
+        ssd1306_draw_string(&ssd, "Desloc: 20m/s@", 10, 30);
 
         ssd1306_send_data(&ssd);
     }
